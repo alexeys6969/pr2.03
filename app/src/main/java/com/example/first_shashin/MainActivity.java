1ad844373fe671de140851624af387d08a581f12
Linashihova <linashikhova@mail.ru>
2026-02-20 15:29:05 +0500

    asd

--- app/src/main/java/com/example/first_shashin/MainActivity.java
+++ app/src/main/java/com/example/first_shashin/MainActivity.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +18,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void OnStepAge(View view){
+    public void OnStepName(View view){
+        EditText _name = findViewById(R.id.editTextText);
+        String strName = _name.getText().toString();
+
         setContentView(R.layout.step_2);
+
+        TextView tv = findViewById(R.id.textView2);
+        tv.setText("Очень приятно познакомиться "+strName+".\nМеня зовут Бисквит.\n Сколько тебе лет?");
+    }
+    public void OnStepAge(View view){
+        EditText _age = findViewById(R.id.editTextText3);
+        String strAge = _age.getText().toString();
+
+        setContentView(R.layout.step_3);
+
+        TextView tv = findViewById(R.id.textView3);
+        tv.setText("Ничего себе ты большой, целых "+ strAge +".\nКакого ты пола");
     }
     public void OnStepSex(View view){
         setContentView(R.layout.step_3);
