6d323879a9904132d7ce6429ef72632d30d7afea
Linashihova <linashikhova@mail.ru>
2026-02-20 14:54:49 +0500

    реализованный переход. Фамилия

--- app/src/main/java/com/example/first_shashin/MainActivity.java
+++ app/src/main/java/com/example/first_shashin/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+    }
 
+    public void OnStepAge(View view){
+        setContentView(R.layout.step_2);
+    }
+    public void OnStepSex(View view){
+        setContentView(R.layout.step_3);
+    }
+    public void OnStepEmail(View view){
+        setContentView(R.layout.step_4);
     }
 }
